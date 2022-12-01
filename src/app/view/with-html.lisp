ee60888e0121a62c7104411659b7b2117a4730e0
Juan Reynoso Elias <juan@ikkiware.com>
2022-12-01 17:50:38 -0600

    Update Bootstrap v5.2.3

--- src/app/view/with-html.lisp
+++ src/app/view/with-html.lisp
@@ -11,7 +11,7 @@
 		      :content "width=device-width, initial-scale=1")
 	      (<:link :rel "stylesheet"
 		      :type "text/css"
-		      :href "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css")
+		      :href "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css")
 	      (<:script :type "text/javascript"
 			:src "https://code.jquery.com/jquery-3.6.0.min.js")
 	      (<:script :type "text/javascript"
